aa03155bc2188e00429f7891ec46df59bb5c7945
Pablo Ludena <pablovinlud90@gmail.com>
2024-10-24 22:44:40 -0500

    Proyecto base del control de inventario

---
+++ src/main/java/com/company/risteck/InventorySystemFApplication.java
@@ -0,0 +1,13 @@
+package com.company.risteck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySystemFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySystemFApplication.class, args);
+	}
+
+}
